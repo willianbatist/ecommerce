8fd9f371c54d17274d1f48de8fc801a1576d87a4
Willian Batista <willian.alves.b15@gmail.com>
2024-03-12 21:03:04 -0300

    fix: create user

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,14 +1,35 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserCreateDTO } from './dto/user.dto';
+import { Response } from 'express';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('/user')
-  async create(@Body() body: UserCreateDTO) {
-    const user = await this.userService.create(body);
-    return user;
+  @Post()
+  async create(@Body() body: UserCreateDTO, @Res() res: Response) {
+    try {
+      const user = await this.userService.create(body);
+      return res.status(HttpStatus.CREATED).json(user);
+    } catch (error) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          error: 'error creating user',
+        },
+        HttpStatus.BAD_REQUEST,
+        {
+          cause: error,
+        },
+      );
+    }
   }
 }
