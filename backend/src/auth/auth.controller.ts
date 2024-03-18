340a7b45cfa7dd564fe680f91672e060e9aac205
Willian Alves Batista <willian.alves.b15@gmail.com>
2024-03-18 20:58:27 -0300

    feat(auth): implement 'login' generating JWT

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,17 +4,20 @@ import {
   Post,
   HttpStatus,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  async Login() {
-    // return this.authService.login();
+  async Login(@Request() req: AuthRequest) {
+    return this.authService.login(req.user);
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { UnauthorizedError } from './errors/unauthorized.error';
+import { User } from 'src/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findUserEmail(email);
@@ -21,5 +28,19 @@ export class AuthService {
     }
 
     throw new UnauthorizedError('Usuário e/ou senha inválidos');
+  }
+
+  async login(user: User): Promise<UserToken> {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
   }
 }
